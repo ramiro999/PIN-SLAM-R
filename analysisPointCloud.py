f6e48b84ad39dd1bf352bf557cb30f5bc3d55929
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-10-17 16:40:56 -0500

    add moduls analysis

--- analysisPointCloud.py
+++ analysisPointCloud.py
@@ -41,38 +41,4 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 ax.set_title('Nube de puntos - hesai/pandar')
-
-#plt.show()
-
-
-# Extraer datos del tópico de datos IMU
-imu_data = bag.message_by_topic('/imu/data')
-
-df = pd.DataFrame(imu_data)
-
-print(df.head())
-print(df.describe())
-
-
-print(f"Datos extraídos en: {imu_data}")
-
-# leer los datos del tópico de datos IMU
-with rosbag.Bag('config/lidar_hesai/comedorCompleto.bag') as bag:
-    for topic, msg, t in bag.read_messages(topics=['/imu/data']):
-        print(f"Topic: {topic}")
-        print(f"Message: {msg}")
-        print(f"Time: {t}")
-        break
-
-# Graficar los datos de aceleración (ax, ay, az)
-import matplotlib.pyplot as plt
-
-plt.figure()
-plt.plot(df['Time'], df['linear_acceleration.x'], label='Acc X')
-plt.plot(df['Time'], df['linear_acceleration.y'], label='Acc Y')
-plt.plot(df['Time'], df['linear_acceleration.z'], label='Acc Z')
-plt.xlabel('Tiempo (s)')
-plt.ylabel('Aceleración (m/s²)')
-plt.title('Aceleración IMU')
-plt.legend()
 plt.show()
