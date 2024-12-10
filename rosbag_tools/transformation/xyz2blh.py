111fd823bb926e05777f4d86442e5770130e45cb
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-12-10 13:12:34 -0500

    add analysisKML and transformation pruebas

--- rosbag_tools/transformation/xyz2blh.py
+++ rosbag_tools/transformation/xyz2blh.py
@@ -1,11 +1,12 @@
 import argparse
-import math 
+import math
+import open3d as o3d
 
 A = 6378137.0
 B = 6356752.314245
 
 def xyz2blh(x, y, z):
-    e = math.sqrt(1 - (B**2)/(A**2))
+    e = math.sqrt(1 - (B**2) / (A**2))
     longitude = math.atan2(y, x)
     xy_hypot = math.hypot(x, y)
     lat0 = 0
@@ -16,7 +17,7 @@ def xyz2blh(x, y, z):
         latitude = math.atan((z + e**2 * N * math.sin(lat0)) / xy_hypot)
     N = A / math.sqrt(1 - e**2 * math.sin(latitude)**2)
     if abs(latitude) < math.pi / 4:
-        R, phi = math.hypot(xy_hypot, z), math_atan(z / xy_hypot)
+        R, phi = math.hypot(xy_hypot, z), math.atan(z / xy_hypot)
         height = R * math.cos(phi) / math.cos(latitude - N)
     else:
         height = z / math.sin(latitude) - N * (1 - e**2)
@@ -25,24 +26,27 @@ def xyz2blh(x, y, z):
     return latitude, longitude, height
 
 def read_pcd(file_path):
-    return pcl.load(file_path)
+    return o3d.io.read_point_cloud(file_path)
 
 def write_pcd(file_path, cloud):
-    pcl.save(cloud, file_path)
+    o3d.io.write_point_cloud(file_path, cloud)
 
 def transform_and_save_pcd(input_pcd, output_pcd):
     cloud = read_pcd(input_pcd)
-    transformed_cloud = []
+    points = cloud.points  
+    transformed_points = []
 
-    for point in cloud:
+    for point in points:
         x, y, z = point[0], point[1], point[2]
         lat, lon, hgt = xyz2blh(x, y, z)
-        transformed_cloud.append([lat, lon, hgt])
+        transformed_points.append([lat, lon, hgt])
 
-    transformed_cloud = pcl.PointCloud(transformed_cloud)
+    # Crear un nuevo PointCloud con los puntos transformados
+    transformed_cloud = o3d.geometry.PointCloud()
+    transformed_cloud.points = o3d.utility.Vector3dVector(transformed_points)
     write_pcd(output_pcd, transformed_cloud)
 
 if __name__ == "__main__":
-    input_pcd = "input.pcd"
-    output_pcd = "output.pcd"
+    input_pcd = "./filesPCD/globalmap_lidar_feature.pcd"
+    output_pcd = "./filesPCD/output.pcd"
     transform_and_save_pcd(input_pcd, output_pcd)
